8572f14acca4d5b82887409c626bab4740a66627
Peter Townsend <karatarak@protonmail.com>
2021-03-27 21:00:26 -0400

    Fixed unit tests

--- test/Todo.Tests/ItemRepository_GetItems.cs
+++ test/Todo.Tests/ItemRepository_GetItems.cs
@@ -8,25 +8,25 @@ namespace Todo.Tests
     public class ItemsRepository_GetItems
     {
         [Fact]
-        public async Task GetByUserId_BoardIdNull_Returns3()
+        public async Task GetByUserId_BoardIdNull_Returns8()
         {
             var userId = ItemsRepository.DefaultUserId;
             var repository = new ItemsRepository();
 
             var items = await repository.GetByUserId(userId, null);
 
-            Assert.Equal(3, items.Count);
+            Assert.Equal(8, items.Count);
         }
 
         [Fact]
-        public async Task GetByUserId_BoardIdDefault_Returns1()
+        public async Task GetByUserId_BoardIdDefault_Returns3()
         {
             var userId = ItemsRepository.DefaultUserId;
             var repository = new ItemsRepository();
 
             var items = await repository.GetByUserId(userId, ItemsRepository.DefaultBoardId);
 
-            Assert.Equal(2, items.Count);
+            Assert.Equal(3, items.Count);
         }
 
         [Fact]
@@ -37,7 +37,7 @@ namespace Todo.Tests
 
             var items = await repository.GetByUserId(userId, null);
 
-            Assert.Equal(3, items.Count);
+            Assert.True(items[0].due_date < items[1].due_date);
         }
     }
 }
